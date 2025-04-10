6be9b1a3263adc353baae750f9ffa44ffdf9ada3
Codster177 <codster157@gmail.com>
2025-04-10 01:36:51 -0400

    Footer tweaks

--- next-app/app/components/PageWrapper/Footer/Footer.tsx
+++ next-app/app/components/PageWrapper/Footer/Footer.tsx
@@ -1,27 +1,52 @@
 import React from "react";
+import NavBarIcon from "../NavBar/NavBarIcon";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
+      <aside>
+        <Link href="./">
+          <h1 className="footer-title link link-hover pt-10">
+            cody-taylor.com
+          </h1>
+        </Link>
+        <a className="link link-hover">Resume</a>
         <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
+      </aside>
+      <nav>
+        <Link href="/projects">
+          <h2 className="footer-title link link-hover pt-10">Projects</h2>
+        </Link>
+        <a className="link link-hover">Time Management Project</a>
+        <a className="link link-hover">D&D Project</a>
+        <a className="link link-hover">Symptomatch</a>
       </nav>
       <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
+        {/* <Link href="/projects"> */}
+        <h2 className="footer-title link link-hover pt-10">Games</h2>
+        {/* </Link> */}
+        <a className="link link-hover">Boomerang Bunny</a>
+        <a className="link link-hover">Growing Up</a>
+        <a className="link link-hover">Floral Felines</a>
+        <a className="link link-hover">Stray Spirit</a>
+      </nav>
+      <nav>
+        <h2 className="footer-title pt-10">Socials</h2>
+        <div className="grid grid-flow-col gap-4">
+          <NavBarIcon
+            icon="/Icons/LinkedIn_Icon.svg/"
+            linkString="https://www.linkedin.com/in/cody-taylor-36a6aa24b/"
+          ></NavBarIcon>
+          <NavBarIcon
+            icon="/Icons/Itch-Icon.svg/"
+            linkString="https://codster177.itch.io/"
+          ></NavBarIcon>
+          <NavBarIcon
+            icon="/Icons/Github-Icon.svg/"
+            linkString="https://github.com/Codster177"
+          ></NavBarIcon>
+        </div>
       </nav>
     </footer>
   );
--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -4,7 +4,6 @@ import CenterPiece from "./components/CenterPiece/CenterPiece";
 import StoryCard from "./components/StoryCards/StoryCard";
 import PageWrapper from "./components/PageWrapper/PageWrapper";
 
-
 export default function Home() {
   return (
     <>
@@ -13,27 +12,13 @@ export default function Home() {
         <div className="divider" />
         <StoryCard
           title="About Me"
-          image="/Pictures/Avira Astaria Done.png"
+          image="/Pictures/Disney Photo.jpg"
           textRight={true}
         >
-          I'm a student at the University of Central Florida studying Computer
-          Science and I have a strong love for programming and game development.
-          I am very passionate, determined, and strong willed when pursuing work
-          with computer science, as I find it extremely joyous to see a project
-          I've worked on come to life. I especially enjoy projects that
-          challenge me creatively.
-        </StoryCard>
-        <StoryCard
-          title="About Me"
-          image="https://media.licdn.com/dms/image/v2/D4D12AQFCBd_by-z9dw/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1706851421227?e=2147483647&v=beta&t=K_IdssydSB6cneMqKEpDC_BGVFRj73ee9ql-vD5IiRs"
-          textRight={false}
-        >
-          I'm a student at the University of Central Florida studying Computer
-          Science and I have a strong love for programming and game development.
-          I am very passionate, determined, and strong willed when pursuing work
-          with computer science, as I find it extremely joyous to see a project
-          I've worked on come to life. I especially enjoy projects that
-          challenge me creatively.
+          I'm a currently a junior at the University of Central Florida studying
+          Computer Science. I have strong interests in full stack development,
+          software engineering, machine learning, and game development. I
+          absolutely love seeing a project come together.
         </StoryCard>
       </PageWrapper>
     </>
